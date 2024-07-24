b3ca6936f2567df1ad92b48f838321174fd4bca5
Dan1lk <prem_tank99@mail.ru>
2024-07-24 15:59:30 +0300

    Написали функцию sales_over_time(sales_data), которая считает общую сумму продаж за определенную дату

--- core.py
+++ core.py
@@ -1,8 +1,9 @@
-dct = {}
 lst = []
+dct = {}
 dct2 = {}
+dct3 = {}
 def read_sales_data(file_path: str):
-    '''Сохраняем покупки в список словарей, возвращаем этот список продаж'''
+    '''Сохраняем покупки в список словарей, возвращаем этот список продаж.'''
 
     with open(file_path, 'r', encoding="utf-8") as file:
         for row in file.readlines():
@@ -15,13 +16,25 @@ def read_sales_data(file_path: str):
         return lst
 
 def total_sales_per_product(sales_data):
-    """ Принимает список продаж и возвращает словарь, где ключ - название продукта,
-    а значение - общая сумма продаж этого продукта."""
+    ''' Принимает список продаж и возвращает словарь, где ключ - название продукта,
+    а значение - общая сумма продаж этого продукта.'''
     for row in sales_data:
         try:
             dct2[row['название']] += int(row['количество']) * int(row['цена'])
         except KeyError:
             dct2[row['название']] = int(row['количество']) * int(row['цена'])
     return dct2
-read_sales_data('sales.txt')
-total_sales_per_product(lst)
+
+def sales_over_time(sales_data):
+    '''Считаем общую сумму продаж за определенную дату.'''
+
+    for row in sales_data:
+        try:
+            dct3[row['дата']] += int(row['количество']) * int(row['цена'])
+        except KeyError:
+            dct3[row['дата']] = int(row['количество']) * int(row['цена'])
+    return dct3
+
+print(read_sales_data('sales.txt'))
+print(total_sales_per_product(lst))
+print(sales_over_time(lst))
